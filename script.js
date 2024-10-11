243072ee0f254d3f7271f46ff1ea93cd42c68037
Haoz-Rundrego <fanathl6@gmail.com>
2024-10-11 20:42:55 +0300

    V0.2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let block = document.querySelector(".block");
+let box1 = document.querySelector(".p1");
 document.addEventListener("mousemove", function (e) {
-  let dx = e.pageX - window.innerWidth / 2;
-  let dy = e.pageY - window.innerHeight / 2;
-  let angleX = (20 * dx) / window.innerWidth / 2;
-  let angleY = (20 * dy) / window.innerHeight / 2;
-  block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`;
+  let rx = e.pageX - window.innerWidth / 4;
+  let ry = e.pageY - window.innerHeight / 4;
+  let radX = (20 * rx) / window.innerWidth / 4;
+  let radY = (20 * ry) / window.innerHeight / 4;
+  box1.style.transform = `rotateX(${-radY}deg) rotateY(${radX}deg)`;
+});
+
+let box2 = document.querySelector(".p2");
+document.addEventListener("mousemove", function (e) {
+  let dx = e.pageX - window.innerWidth / 7;
+  let dy = e.pageY - window.innerHeight / 7;
+  let degX = (20 * dx) / window.innerWidth / 7;
+  let degY = (20 * dy) / window.innerHeight / 7;
+  box2.style.transform = `rotateX(${-degY}deg) rotateY(${degX}deg)`;
 });
